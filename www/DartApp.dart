9595050319076020f240339815de3c5077cf95ca
Atikin-art <nekhnikita909@gmail.com>
2025-04-01 16:12:54 +0200

    Nessim le noob

--- www/DartApp.dart
+++ www/DartApp.dart
@@ -1,10 +1,11 @@
+
 void main() {
   final date = DateTime.now();
   for (var i = 0; i < 5; i++) {
-    print('hello world' + i.toString());
+    
     print(date);
   }
 
-  var List = ['Nathael', 'Noé', 'Yann'];
+  var List = [''];
   print(List);
-}
+} 
