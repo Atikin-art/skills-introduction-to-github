8597e9cc40ab013af933b20311ba146107dd9570
Atikin-art <nekhnikita909@gmail.com>
2025-04-07 20:24:49 +0200

    Update DartApp.dart

--- www/DartApp.dart
+++ www/DartApp.dart
@@ -1,11 +1,53 @@
-
 void main() {
-  final date = DateTime.now();
-  for (var i = 0; i < 5; i++) {
-    
-    print(date);
+  var sanya = Human(name: "Sacha", age: 23, height: 190);
+
+  print(sanya.name);
+
+  var vova = Human(name: "Vova", age: 23, height: 190);
+  print(vova.name);
+  print(Human.count);
+
+  final Animal wolf = Wolf(5);
+  final tiger = Tiger(1);
+  print(tiger.age);
+  print(wolf.age);
+}
+
+class Human {
+  Human({required this.name, required this.age, required this.height}) {
+    count += 1;
   }
 
-  var List = [''];
-  print(List);
-} 
+  static int count = 0;
+
+  final String name;
+  int age;
+  double height;
+}
+
+abstract interface class Animal {
+  double get age;
+  void makeSound();
+}
+
+class Wolf implements Animal {
+  Wolf(this.age);
+  @override
+  void makeSound() {
+    print('Auuuuuuuu');
+  }
+
+  @override
+  final double age;
+}
+
+class Tiger implements Animal {
+  Tiger(this.age);
+  @override
+  void makeSound() {
+    print('Rrrrrrr');
+  }
+
+  @override
+  final double age;
+}
